5330444ffb20fa629d0e67f020c627aee26fa360
Oleksii Salkov <apxucmpax@gmail.com>
2022-08-07 20:06:20 +0300

    v2.26.1 change version

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,7 +7,7 @@ let online = false;
 let sentStatus = false;
 // flag open modal groups
 let isOpenModalGroups = false;
-const version = '2.26.0';
+const version = '2.26.1';
 /** instanceService is now Service
  * @member {Service} instanceService
  */
